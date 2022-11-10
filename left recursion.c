b74bc17a28f48cb28eb85e1f1a5fd1cd40010743
Rashidulhaq <77320958+Rashidulhaq@users.noreply.github.com>
2022-11-10 18:42:41 +0600

    Update left recursion.c

--- left recursion.c
+++ left recursion.c
@@ -1,37 +1,58 @@
   #include<stdio.h>
   #include<string.h>
   #define SIZE 10
-  int main () {
-       char non_terminal;
-       char beta,alpha;
-       int num;
-       char production[10][SIZE];
-       int index=3; /* starting of the string following "->" */
-       printf("Enter Number of Production : ");
+  int main ()
+  {
+
+
+    char non_terminal;
+    char beta,alpha;
+
+    int num;
+
+    char production[10][SIZE];
+
+    int index=3; /* starting of the string following "->" */
+
+printf("how many recursion? enter number of production  : ");
+
        scanf("%d",&num);
-       printf("Enter the grammar as E->E-A :\n");
+       printf(" enter your grammer :\n");
+
        for(int i=0;i<num;i++){
             scanf("%s",production[i]);
        }
+
+
        for(int i=0;i<num;i++){
-            printf("\nGRAMMAR : : : %s",production[i]);
-            non_terminal=production[i][0];
+
+       printf("\nGRAMMAR : : : \t %s",production[i]);
+    non_terminal=production[i][0];
+
             if(non_terminal==production[i][index]) {
-                 alpha=production[i][index+1];
-                 printf(" is left recursive.\n");
-                 while(production[i][index]!=0 && production[i][index]!='|')
+
+    alpha=production[i][index+1];
+    printf(" is left recursive.\n");
+
+  while(production[i][index]!=0 && production[i][index]!='|')
                       index++;
-                 if(production[i][index]!=0) {
-                      beta=production[i][index+1];
-                      printf("Grammar without left recursion:\n");
-                      printf("%c->%c%c\'",non_terminal,beta,non_terminal);
-                      printf("\n%c\'->%c%c\'|E\n",non_terminal,alpha,non_terminal);
+
+    if(production[i][index]!=0) {
+        beta=production[i][index+1];
+        printf("Grammar without left recursion:\n");
+        printf("%c->%c%c\'",non_terminal,beta,non_terminal);
+        printf("\n%c\'->%c%c%c\'|$\n",non_terminal,alpha,beta,non_terminal);
                  }
-                 else
-                      printf(" can't be reduced\n");
-            }
+
+
+        else
+      printf(" can't be reduced\n");
+      }
+
+
             else
                  printf(" is not left recursive.\n");
             index=3;
        }
  }
+
